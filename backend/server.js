e5fb5bc81c03bf448b2f10a49a51538e6365faf5
rekha0suthar <rekha0suthar@gmail.com>
2024-11-06 18:13:46 +0530

    vercek.json file added

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,7 @@ app.use('/api/blogs', blogRouter);
 
 // MongoDB connection
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
